5a67d8904cb36ba49a39c700e6aa20acafd0bad1
Ed Hone <edwardhone@gmail.com>
2023-05-15 12:45:55 +0100

    remove unused functions

--- results_app/plot.R
+++ results_app/plot.R
@@ -131,50 +131,6 @@ create_volcano_plot <- function(df){
 }
 
 
-create_exposure_volcano_plot <- function(df){
-  pthreshold_rank <- rank(-log10(df$p))[which.min(abs(df$p-0.05))]-1
-  adj_pthreshold <- 0.05/nrow(df)
-  adj_pthreshold_rank <- rank(-log10(df$p))[which.min(abs(df$p-adj_pthreshold))]-1
-  Plot <- ggplot(df,
-                 aes(Estimate=est,P=p,Outcome=outcome_linker,Cohorts=cohorts,N=total_n,
-                     x=est_SDM,y=rank(-log10(p)),Exposure=exposure_linker
-                 ))+
-    geom_point(aes(colour=outcome_class),size=0.5,alpha=0.5)+
-    geom_vline(xintercept = 0,colour="grey40")+
-    theme_classic()+
-    scale_colour_brewer(palette = graph_colours)+
-    xlab("Standardised effect estimate")+
-    ylab("Ranked -log10(P)")+
-    facet_grid(.~person_exposed)+  
-    coord_cartesian(xlim=c(-0.75,0.75))+
-    geom_hline(yintercept = pthreshold_rank,linetype="dashed",colour="blue")+
-    geom_hline(yintercept = adj_pthreshold_rank,linetype="dashed",colour="red")
-  ggplotly(Plot,tooltip=c("P","Estimate","Outcome","Exposure","Cohorts","N"))
-}
-
-# by outcome
-
-create_outcome_volcano_plot <- function(df){
-  pthreshold_rank <- rank(-log10(df$p))[which.min(abs(df$p-0.05))]-1
-  adj_pthreshold <- 0.05/nrow(df)
-  adj_pthreshold_rank <- rank(-log10(df$p))[which.min(abs(df$p-adj_pthreshold))]-1
-  Plot <- ggplot(df,
-                 aes(Estimate=est,P=p,Outcome=outcome_linker,Cohorts=cohorts,N=total_n,
-                     x=est_SDM,y=rank(-log10(p)),Exposure=exposure_linker
-                 ))+
-    geom_point(aes(colour=exposure_class),size=0.5,alpha=0.5)+
-    geom_vline(xintercept = 0,colour="grey40")+
-    theme_classic()+
-    scale_colour_brewer(palette = graph_colours)+
-    xlab("Standardised effect estimate")+
-    ylab("Ranked -log10(P)")+
-    facet_grid(.~person_exposed)+  
-    coord_cartesian(xlim=c(-0.75,0.75))+
-    geom_hline(yintercept = pthreshold_rank,linetype="dashed",colour="blue")+
-    geom_hline(yintercept = adj_pthreshold_rank,linetype="dashed",colour="red")
-  ggplotly(Plot,tooltip=c("P","Estimate","Outcome","Exposure","Cohorts","N"))
-}
-
 #######################
 # Coef plot functions #
 #######################
